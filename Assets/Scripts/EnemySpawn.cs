c0abfb15dc0b6af8a6df7a742f578805a89c07c4
krzysztofskorek <krzysztofskorek1@gmail.com>
2024-12-02 21:09:38 +0100

    added scenario class

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -7,9 +7,10 @@ public class EnemySpawn : MonoBehaviour
     public Vector3[] spawnPoints;
 
     int index = 0;
-    // Start is called before the first frame update
+    
     void Start()
     {
+        
         InvokeRepeating(nameof(SpawnEnemy), 0.1f, 2f);
     }
 
